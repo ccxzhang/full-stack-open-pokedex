89ca62b63330efa62f22c7fea562031b2a8703ff
Charlie Zhang <cz317@georgetown.edu>
2024-06-05 01:08:13 -0400

    Fix exercise 11.9

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const PORT = process.env.PORT || 5001
 
 app.use(express.static('dist'))
 
+app.get('/health', (req, res) => {
+  res.send('ok 200 test test new branch again 5')
+})
+
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`)
 })
